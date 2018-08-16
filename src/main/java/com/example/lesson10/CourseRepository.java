e31696d3acc88052620d5f735d79bd013145486d
Mona Lisa <josemyjoseph@gmail.com>
2018-08-16 13:31:13 -0400

    initial commit

---
+++ src/main/java/com/example/lesson10/CourseRepository.java
@@ -0,0 +1,6 @@
+package com.example.lesson10;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, Long> {
+}
---
+++ src/main/java/com/example/lesson10/Lesson10Application.java
@@ -0,0 +1,14 @@
+package com.example.lesson10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson10Application {
+
+//  Complete data life cycle – input, update, list, detail, delete
+
+  public static void main(String[] args) {
+    SpringApplication.run(Lesson10Application.class, args);
+  }
+}
